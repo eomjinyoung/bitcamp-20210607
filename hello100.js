41dba7513d027858330c94318a8ff84aab9fde8b
gksqlc6401 <gksqlc6401@naver.com>
2021-06-09 16:36:43 +0900

    ,

--- hello100.js
+++ hello100.js
@@ -1,9 +1,2 @@
-
-console.log("테스트1");
-console.log("테스트2");
-console.log("테스트3");
-console.log("테스트4");
-console.log("테스트12233");
-console.log("테스트12233");
-console.log("테스트12233");
+console.log("테스dfdfdf1");
 
--- hello103.js
+++ hello103.js
@@ -1,9 +1,3 @@
 
 console.log("테스트1");
-console.log("테스트2");
-console.log("테스트3");
-console.log("테스트4");
-console.log("테스트5");
-console.log("테스트6");
-console.log("테스트7");
 
