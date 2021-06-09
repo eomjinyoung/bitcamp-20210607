595307f31149259a0d35df26af2b01b5dde5e359
eomjinyoung <jinyoung.eom@gmail.com>
2021-06-09 12:34:23 +0900

    .

--- hello2.c
+++ hello2.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
 int main() {
-<<<<<<< HEAD
     printf("Hello, world!");
-=======
-    printf("Hello, world2!");
->>>>>>> 89ee1711fee40aa2ec7b307ba94eb5f085d652ac
     return 0;
 }
