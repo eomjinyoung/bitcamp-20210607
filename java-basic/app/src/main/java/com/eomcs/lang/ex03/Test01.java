fc847efe90a358d1bc6453d6483357b1a1f2e1f6
eomjinyoung <jinyoung.eom@gmail.com>
2021-06-11 17:56:41 +0900

    .

---
+++ java-basic/app/src/main/java/com/eomcs/lang/ex03/Test01.java
@@ -0,0 +1,12 @@
+package com.eomcs.lang.ex03;
+
+class Test01 {
+    public static void main(String[] args) {
+        System.out.println(100);
+        System.out.println(3.14);
+        System.out.println('A');
+        System.out.println('가');
+        System.out.println(true);
+        System.out.println("ABC가각간");
+    }
+}
---
+++ java-basic/app/src/main/java/com/eomcs/lang/ex03/Test02.java
@@ -0,0 +1,8 @@
+package com.eomcs.lang.ex03;
+
+class Test02 {
+    public static void main(String[] args) {
+        System.out.println(100);
+        System.out.println(100);
+    }
+}
